8b389bd118420bad8442e19a8482c597e8a9748c
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-09-29 18:34:44 +0100

    Improved accessibility for on-screen readers by making a clickable text available ontop of the input (i.e Top Text & Bottom Text). Also added some changes to the CSS.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,68 +1,74 @@
-import React from "react"
+import React from "react";
 
 export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemes, setAllMemes] = React.useState([])
-    
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+    const [allMemes, setAllMemes] = React.useState([]);
+
     React.useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
-    }, [])
-    
+            .then((res) => res.json())
+            .then((data) => setAllMemes(data.data.memes));
+    }, []);
+
     function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length)
-        const url = allMemes[randomNumber].url
-        setMeme(prevMeme => ({
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        const url = allMemes[randomNumber].url;
+        setMeme((prevMeme) => ({
             ...prevMeme,
             randomImage: url
-        }))
-        
+        }));
     }
-    
+
     function handleChange(event) {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
+        const { name, value } = event.target;
+        setMeme((prevMeme) => ({
             ...prevMeme,
             [name]: value
-        }))
+        }));
     }
-    
+
     return (
         <main>
             <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
-                    Generate a new meme
-                </button>
-            </div>
+    <div>
+        <label htmlFor="topText" className="form--label">
+            Top Text
+        </label>
+        <input
+            type="text"
+            id="topText"
+            name="topText"
+            className="form--input"
+            value={meme.topText}
+            onChange={handleChange}
+        />
+    </div>
+    <div>
+        <label htmlFor="bottomText" className="form--label">
+            Bottom Text
+        </label>
+        <input
+            type="text"
+            id="bottomText"
+            name="bottomText"
+            className="form--input"
+            value={meme.bottomText}
+            onChange={handleChange}
+        />
+    </div>
+    <button className="form--button" onClick={getMemeImage}>
+        Generate a new meme
+    </button>
+</div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" alt="Generated Meme" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
-    )
+    );
 }
